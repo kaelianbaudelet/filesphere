b239b56aab4ce683a48593fb795cbcdb4cd751a2
Kaelian BAUDELET <contact@kaelian.dev>
2025-03-24 11:38:41 +0100

    test

--- public/assets/js/offcanvas.js
+++ public/assets/js/offcanvas.js
@@ -1,11 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const toggleNavigationButtons =
-        document.querySelectorAll(".toggle-navigation");
-    const bodyNavigation = document.querySelector(".app-body-navigation");
+try {
+    document.addEventListener("DOMContentLoaded", function () {
+        const toggleNavigationButtons =
+            document.querySelectorAll(".toggle-navigation");
+        const bodyNavigation = document.querySelector(".app-body-navigation");
 
-    toggleNavigationButtons.forEach(function (button) {
-        button.addEventListener("click", function () {
-            bodyNavigation.classList.toggle("open");
+        toggleNavigationButtons.forEach(function (button) {
+            button.addEventListener("click", function () {
+                bodyNavigation.classList.toggle("open");
+            });
         });
     });
-});
+} catch (error) {}
--- public/assets/js/uploadarea.js
+++ public/assets/js/uploadarea.js
@@ -1,41 +1,45 @@
-const dropArea = document.getElementById("drop-area");
-const fileInput = document.getElementById("file");
-const fileInfo = document.getElementById("file-info");
+try {
+    const dropArea = document.getElementById("drop-area");
+    const fileInput = document.getElementById("file");
+    const fileInfo = document.getElementById("file-info");
 
-dropArea.addEventListener("dragover", (event) => {
-    event.preventDefault();
-    dropArea.classList.add("dragging");
-});
+    if (dropArea && fileInput && fileInfo) {
+        dropArea.addEventListener("dragover", (event) => {
+            event.preventDefault();
+            dropArea.classList.add("dragging");
+        });
 
-dropArea.addEventListener("dragleave", () => {
-    dropArea.classList.remove("dragging");
-});
+        dropArea.addEventListener("dragleave", () => {
+            dropArea.classList.remove("dragging");
+        });
 
-dropArea.addEventListener("drop", (event) => {
-    event.preventDefault();
-    dropArea.classList.remove("dragging");
-    const files = event.dataTransfer.files;
-    if (files.length > 10) {
-        alert("Vous ne pouvez téléverser que 10 fichiers au maximum.");
-    } else {
-        fileInput.files = files;
-        updateFileInfo(files);
+        dropArea.addEventListener("drop", (event) => {
+            event.preventDefault();
+            dropArea.classList.remove("dragging");
+            const files = event.dataTransfer.files;
+            if (files.length > 10) {
+                alert("Vous ne pouvez téléverser que 10 fichiers au maximum.");
+            } else {
+                fileInput.files = files;
+                updateFileInfo(files);
+            }
+        });
+
+        fileInput.addEventListener("change", function () {
+            if (this.files.length > 10) {
+                alert("Vous ne pouvez téléverser que 10 fichiers au maximum.");
+                this.value = "";
+                fileInfo.textContent = "";
+            } else {
+                updateFileInfo(this.files);
+            }
+        });
+
+        function updateFileInfo(files) {
+            const fileNames = Array.from(files)
+                .map((file) => file.name)
+                .join(", ");
+            fileInfo.textContent = `${files.length} fichier(s) sélectionné(s): ${fileNames}`;
+        }
     }
-});
-
-fileInput.addEventListener("change", function () {
-    if (this.files.length > 10) {
-        alert("Vous ne pouvez téléverser que 10 fichiers au maximum.");
-        this.value = "";
-        fileInfo.textContent = "";
-    } else {
-        updateFileInfo(this.files);
-    }
-});
-
-function updateFileInfo(files) {
-    const fileNames = Array.from(files)
-        .map((file) => file.name)
-        .join(", ");
-    fileInfo.textContent = `${files.length} fichier(s) sélectionné(s): ${fileNames}`;
-}
+} catch (error) {}
