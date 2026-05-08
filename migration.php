eecde753400cad2a12486dd2a4a02fd3951ccb25
Kaelian Baudelet <aconique@gmail.com>
2026-05-08 18:15:39 +0200

    fix: ci

--- migration.php
+++ migration.php
@@ -6,7 +6,7 @@ use Dotenv\Dotenv;
 require 'vendor/autoload.php';
 
 $dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+$dotenv->safeLoad();
 
 echo "Migration de la base de données\n";
 
